71324270b1611ac05e1acd181103201c0ad5d26a
SashaRez <hellosecondpeople@gmail.com>
2023-07-30 19:24:26 +0300

    first commit

--- CarBaseFinal/Data/ApplicationContext.cs
+++ CarBaseFinal/Data/ApplicationContext.cs
@@ -10,10 +10,6 @@ namespace CarBaseFinal.Data
 
         public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options) { }
 
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            optionsBuilder.UseSqlServer("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\Саша\\Documents\\CarBase.mdf;Integrated Security=True;Connect Timeout=30;MultipleActiveResultSets=true");
-        }
 
         public DbSet<Car> Cars { get; set; }
         public DbSet<Korzina> Korzina { get; set; }
